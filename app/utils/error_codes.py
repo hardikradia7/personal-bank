231d738f1b752cac0576ca2649e1cddfbca6f6e6
unknown <hardikradia8720@gmail.com>
2022-05-29 00:05:33 +0530

    Added logic to validate iban endpoint.
    -> Added code logic to validate iban endpoint.
    -> Added code changes to handle exceptions.

---
+++ app/utils/error_codes.py
@@ -0,0 +1,19 @@
+INTERNAL_SERVER_ERROR = {
+    'error_code': '10000',
+    'error_message': 'Failed to perform specified operation. An internal error has occurred.'
+}
+
+ILLEGAL_IBAN_CHARACTER = {
+    'error_code': '10001',
+    'error_message': 'The IBAN number you provided has an illegal character.'
+}
+
+INVALID_IBAN_LENGTH = {
+    'error_code': '10002',
+    'error_message': 'The IBAN number you provided is few characters short.'
+}
+
+INVALID_IBAN = {
+    'error_code': '10003',
+    'error_message': 'The IBAN number you provided is not valid.'
+}
